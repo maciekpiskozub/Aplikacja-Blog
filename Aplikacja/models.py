01f1f1b68e400b0fa44e64617f1ef810c8d8fbf5
MrFajFel <127607303+MrFajFel@users.noreply.github.com>
2024-02-19 18:32:21 +0100

    udana naprawa

--- Aplikacja/models.py
+++ Aplikacja/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    slug = models.SlugField(max_length=250, unique_for_date='published')
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+
+    STATUS_CHOICES = {
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    }
+
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
