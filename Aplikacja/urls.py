47a4a37f6b3d81565cc2e9f1160d6ffaa3ac60b7
Przemek B <pprzemek89@gmail.com>
2024-03-14 15:06:13 +0100

    naprawa2

--- Aplikacja/urls.py
+++ Aplikacja/urls.py
@@ -5,5 +5,8 @@ from Aplikacja import views
 app_name = 'Aplikacja'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>/',
+         views.post_detail,
+         name='post_detail')
 ]
--- Aplikacja/views.py
+++ Aplikacja/views.py
@@ -1,12 +1,23 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from Aplikacja.models import Post
 
 
 def post_list(request):
-    posts = Post.objects.all()
-    return render(request,'blog/post/list.html')
+    posts = Post.objects.all().filter(status='published')
+    return render(request,'blog/post/list.html',
+                  {'posts':posts})
 
+
+
+def post_detail(request,year,month,day,slug):
+    post = get_object_or_404(Post,slug=slug,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request,"blog/post/detail.html",
+    {'post':post})
 
 # Create your views here.
